b744b40ec6edca790276f30cfc3a10b607619172
Mark Wylde <mark@wylde.net>
2025-05-26 13:34:09 +0100

    fix: pages deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,9 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  build: {
+    assets: 'assets'
+  },
+  output: 'static',
   integrations: [react()]
 });
